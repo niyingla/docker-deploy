0a6f5b3d2bed488803324ba271bdc674c37b00c9
微笑只是问候 <1047984367@qq.com>
2020-07-15 23:32:26 +0800

    增加参数

--- course-dubbo-service/build.sh
+++ course-dubbo-service/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-
+#打包jar
 mvn clean package
-
+#打包镜像
 docker build -t hub.mooc.com:8080/micro-service/course-service:latest .
+#上传镜像
 docker push hub.mooc.com:8080/micro-service/course-service:latest
